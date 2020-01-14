7886ce317c6ccf5dbe1f099a21ec8c5f8ac1cfef
Lea De Vylder <leadevylder@icloud.com>
2020-01-14 09:57:35 -0800

    Initial Commit

---
+++ Dicee-IOS13-Swift UI/ContentView.swift
@@ -0,0 +1,66 @@
+//
+//  ContentView.swift
+//  Dicee-IOS13-Swift UI
+//
+//  Created by cate on 11/1/19.
+//  Copyright © 2019 cate. All rights reserved.
+//
+
+import SwiftUI
+
+
+struct ContentView: View {
+    
+    @State var leftDiceNumber = 1
+    @State var rightDiceNumber = 1
+    
+    var body: some View {
+        ZStack {
+            Image("GreenBackground")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+            VStack{
+                Image("DiceeLogo")
+                    Spacer()
+                
+                HStack {
+                    DiceView(n: leftDiceNumber)
+                    DiceView(n: rightDiceNumber)
+                }
+                .padding(.horizontal)
+                Spacer()
+            
+                Button(action: {
+                self.leftDiceNumber = Int.Random(1...6)
+                self.rightDiceNumber = Int.Random(1...6)
+            })
+            {
+                .foregroundColor(.white)
+                .backgroundColor(.red)
+                .frame(width: 80.0, height: 50.0)
+                    .overlay(Text: "Roll")
+                        }
+                     Spacer()
+                        }
+                    }
+                }
+            }
+        
+
+
+    struct DiceView: View {
+        var n: Int
+        var body: some View{
+            Image("dice\(n)")
+            .resizable()
+            .aspectRatio( 1, contentMode: .fit)
+            .padding()
+        }
+    }
+    
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
